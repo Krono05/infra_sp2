071d2503fd61df3777d45e7c1b94492175d1b4e8
Тамир Болданов <rpp-2011@yandex.ru>
2021-06-15 14:07:43 +0300

    first commit

--- api/admin.py
+++ api/admin.py
@@ -2,9 +2,11 @@ from django.contrib import admin
 
 from .models import Category, Genre, Title, User
 
+
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
     list_display = ('pk', 'email', 'bio', 'confirmation_code', 'role')
+
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
